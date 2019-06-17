8faf68a1ff7eac2f4ca4b1d1430baab321ce6780
claudehill <37227828+claudehill@users.noreply.github.com>
2019-06-16 20:02:07 -0400

    remove vue for re-add to main

--- init.js
+++ init.js
@@ -6,3 +6,10 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.dropdown-trigger');
+//   var instances = M.Dropdown.init(elems, options);
+// });
+
+$('.dropdown-trigger').dropdown();
